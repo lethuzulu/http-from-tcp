6083f9e1983e58eed29a3c99f315ab48e599e1cb
Lethu Zulu <lethukuthula.z@instacom.co.za>
2025-11-15 20:25:52 +0200

    Refactor file reading to use a channel for line processing

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,48 @@
+use std::sync::mpsc::{Receiver, channel};
+use std::thread;
 use std::{fs::File, io::Read, str::from_utf8};
 
 fn main() {
-    let mut file = File::open("message.txt").unwrap();
-    let mut buf = [0u8; 8];
-
-    let mut current_line = String::new();
+    let  file = File::open("message.txt").unwrap();
+    let receiver = get_lines_channel(file);
 
     loop {
-        let count = file.read(&mut buf).unwrap();
-        if count == 0 {
-            break;
+        match receiver.recv() {
+            Ok(v) => println!("{}", v),
+            Err(_) => break,
         }
-        let chunk = from_utf8(&buf[0..count]).unwrap();
+    }
+}
 
-        let parts = chunk.split('\n').collect::<Vec<&str>>();
+fn get_lines_channel(mut file: File) -> Receiver<String> {
+    let (sender, receiver) = channel::<String>();
 
-        for part in &parts[0..parts.len() - 1] {
-            current_line.push_str(part);
-            println!("Line {}", current_line);
-            current_line.clear();
+    let _handle = thread::spawn(move || {
+        let mut buf = [0u8; 8];
+
+        let mut current_line = String::new();
+        loop {
+            let size = file.read(&mut buf).unwrap();
+            if size == 0 {
+                break;
+            }
+
+            let chunk = from_utf8(&buf[..size]).unwrap();
+
+            let parts: Vec<&str> = chunk.split('\n').collect();
+
+            for part in &parts[..parts.len() - 1] {
+                current_line.push_str(part);
+                let _ = sender.send(current_line.clone()).unwrap();
+                current_line.clear();
+            }
+
+            current_line.push_str(parts.last().unwrap());
         }
 
-        current_line.push_str(parts.last().unwrap());
-    }
-
-    if !current_line.is_empty() {
-        println!("Line {}", current_line)
-    }
+        if !current_line.is_empty() {
+            let _ = sender.send(current_line).unwrap();
+        }
+    });
+    receiver
 }
